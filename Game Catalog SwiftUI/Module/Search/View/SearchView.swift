fd797bd33427d00afb2a3a948b7fdd6283a5153c
diazagfanuribi <diazagfa@gmail.com>
2020-12-01 13:58:00 +0700

    search page

---
+++ Game Catalog SwiftUI/Module/Search/View/SearchView.swift
@@ -0,0 +1,21 @@
+//
+//  SearchView.swift
+//  Game Catalog SwiftUI
+//
+//  Created by DiazAgfa on 01/12/20.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text("This Feature is still not available yet")
+            .navigationTitle("Search")
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
