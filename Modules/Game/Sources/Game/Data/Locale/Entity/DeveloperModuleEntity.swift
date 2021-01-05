e3029b3d3a8d5aec51d87cc79237b1918da348b2
diazagfanuribi <diazagfa@gmail.com>
2021-01-05 11:34:15 +0700

    untracked

---
+++ Modules/Game/Sources/Game/Data/Locale/Entity/DeveloperModuleEntity.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  
+//
+//  Created by DiazAgfa on 04/01/21.
+//
+
+import Foundation
+import RealmSwift
+
+public class DeveloperModuleEntity: Object {
+  @objc dynamic var ids: Int = 0
+  @objc dynamic var name: String = ""
+  @objc dynamic var imageUrl: String = ""
+
+  public override static func primaryKey() -> String? {
+    return "ids"
+  }
+}
